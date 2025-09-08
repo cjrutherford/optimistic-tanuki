ac221aa17ce9059d8d8eb3333454878e8969fe10
Chris Rutherford <therealchrisrutherford@gmail.com>
2025-09-08 13:12:24 -0400

    updated with images and more design changes for dh

--- apps/digital-homestead/src/app/components/title-bar/title-bar.component.ts
+++ apps/digital-homestead/src/app/components/title-bar/title-bar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeadingComponent, TileComponent } from '@optimistic-tanuki/common-ui'
+import { ButtonComponent, HeadingComponent, TileComponent } from '@optimistic-tanuki/common-ui'
 
 @Component({
   selector: 'dh-title-bar',
-  imports: [CommonModule, HeadingComponent, TileComponent],
+  imports: [CommonModule, HeadingComponent, TileComponent, ButtonComponent],
   templateUrl: './title-bar.component.html',
   styleUrl: './title-bar.component.scss',
 })
-export class TitleBarComponent {}
+export class TitleBarComponent {
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.set(!this.menuOpen());
+  }
+}
