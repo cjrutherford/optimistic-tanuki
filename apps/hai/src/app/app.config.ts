cd5b2609fffbecad6850fe958e87c5ac3209cf51
chris rutherford <therealchrisrutherford@gmail.com>
2026-04-23 19:33:53 -0400

    (chore) cleanup build issues

--- apps/hai/src/app/app.config.ts
+++ apps/hai/src/app/app.config.ts
@@ -1,21 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { appRoutes } from './app.routes';
 import { FontLoadingService, ThemeService } from '@optimistic-tanuki/theme-lib';
-import { provideReturnLinkHandling } from '@optimistic-tanuki/app-registry';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withFetch()),
     provideRouter(appRoutes),
-    provideReturnLinkHandling(),
     ThemeService,
     FontLoadingService,
   ],
