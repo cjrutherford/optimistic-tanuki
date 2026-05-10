11816a1b2bddb125807008674b46759ce37a1a54
chris rutherford <therealchrisrutherford@gmail.com>
2026-05-10 13:37:54 -0400

    fix fin-commander docker build ci

--- libs/theme-ui/src/index.ts
+++ libs/theme-ui/src/index.ts
@@ -1,17 +1,7 @@
 export * from './lib/theme-ui/theme.component';
-export {
-  ThemeCycleTiming,
-  ThemeMotionProfile,
-  ThemeMotionStyle,
-  THEME_LAYER_CLASS_ORDER,
-  ensureThemeLayerOrdering,
-} from '@optimistic-tanuki/theme-lib';
 export * from './lib/theme-ui/palette-selector.component';
 export * from './lib/theme-ui/palette-manager.component';
-export {
-  ThemeHostBindingsDirective,
-  applyThemeToHostBindings,
-} from '@optimistic-tanuki/theme-lib';
+export { ThemeHostBindingsDirective } from '@optimistic-tanuki/theme-lib';
 export { ThemeVariableService } from '@optimistic-tanuki/theme-lib';
 export { ThemeValidationHarnessComponent } from '@optimistic-tanuki/theme-lib';
 export * from './lib/theme-ui/theme-demo.component';
